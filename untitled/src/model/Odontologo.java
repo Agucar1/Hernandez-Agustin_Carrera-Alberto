6adbaa23f0defc48dc7c5b5aa3a274eb4401f577
Alberto Carrera <acarrera_l@hotmail.com>
2024-05-16 19:38:17 -0500

    Agregar modelo

---
+++ untitled/src/model/Odontologo.java
@@ -0,0 +1,37 @@
+package model;
+
+public class Odontologo {
+    private String numeroMatricula;
+    private String nombre;
+    private String apellido;
+
+    public Odontologo(String numeroMatricula, String nombre, String apellido) {
+        this.numeroMatricula = numeroMatricula;
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    public String getNumeroMatricula() {
+        return numeroMatricula;
+    }
+
+    public void setNumeroMatricula(String numeroMatricula) {
+        this.numeroMatricula = numeroMatricula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+}
